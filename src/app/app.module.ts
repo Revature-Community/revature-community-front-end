58eb0448458101b539e505273389fb27ed1277f9
Jun Lei <jlei91@gmail.com>
2021-04-23 09:56:23 -0700

    team_afterhours

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WritepostComponent } from './writepost/writepost.component';
 import { ReadpostComponent } from './readpost/readpost.component';
+import { ReponseComponent } from './reponse/reponse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WritepostComponent,
-    ReadpostComponent
+    ReadpostComponent,
+    ReponseComponent
   ],
   imports: [
     BrowserModule,
