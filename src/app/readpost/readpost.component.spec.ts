ad8774e2735e59ee8097db1fb1fdf3d979495b77
Tevin Gray <tevingray@Tevins-MacBook-Pro.local>
2021-04-23 08:20:10 -0700

    Basic Angular

---
+++ src/app/readpost/readpost.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReadpostComponent } from './readpost.component';
+
+describe('ReadpostComponent', () => {
+  let component: ReadpostComponent;
+  let fixture: ComponentFixture<ReadpostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReadpostComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReadpostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
