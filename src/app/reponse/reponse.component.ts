ac0e7564f46a458f9cfe23a686ec6c1b6ff86b01
Jun Lei <jlei91@gmail.com>
2021-04-26 07:58:00 -0700

    auto resize textbox, submit icon

--- src/app/reponse/reponse.component.ts
+++ src/app/reponse/reponse.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-reponse',
@@ -8,41 +8,52 @@ import { Component, OnInit } from '@angular/core';
 export class ReponseComponent implements OnInit {
   
   constructor() { }
+  @Input() post: number;
+  @ViewChild('textArea', { read: ElementRef }) textArea: ElementRef;
   responses = [];
   responseData = [];
   currentResponse: string;
   ngOnInit(): void {
     const data = [
       {id: 1, content: "this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythisthis is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third hello world", author: "", location: "place 1", post_id: 1},
-      {id: 2, content: "this is a second reply", author: "", location: "place 1",  post_id: 1},
-      {id: 3, content: "this is a third reply", author: "", location: "place 1",  post_id: 1}
+      {id: 2, content: "this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythisthis is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third hello world", author: "", location: "place 1",  post_id: 1},
+      {id: 3, content: "this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythisthis is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third this is a reply this is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third replythis is a third hello world", author: "", location: "place 1",  post_id: 1}
     ]
     this.responseData = data;
     for (let val of data) {
-      let more = false;
+      let lenless255 = false;
       if (val.content.length > 255) {
-        more = true;
+        lenless255 = true;
       }
-      const resp = {id: val.id, response: val.content.slice(0,255), showMore: more};
+      const resp = {id: val.id, response: val.content.slice(0,255), showMore: lenless255};
       this.responses.push(resp);
     }
   }
 
   handleResponses() {
-    const postId = this.responses[0].post_id;
-    const newPost = {id: 4, content: this.currentResponse, post_id: postId};
+    console.log(this.currentResponse)
+    let lenless255 = false;
+    if (this.currentResponse.length > 255) {
+      lenless255 = true;
+    }
+    const postId = this.responseData[0].post_id;
+    const newPost = {id: 4, response: this.currentResponse, post_id: postId, showMore: lenless255};
     this.responses.push(newPost);
-    console.log('added resp', this.responses);
   }
   
-  showMore() {
-    console.log('showme plsz')
-    this.responses[0].response = this.responseData[0].content;
-    this.responses[0].showMore = false;
+  showMore(id) {
+    this.responses[id-1].response = this.responseData[id-1].content;
+    this.responses[id-1].showMore = false;
   }
 
   getDisplayType(type) {
-    console.log(type)
     return type ? 'inline-table' : 'x';
+  }
+
+  autoGrow() {
+    const textArea = this.textArea.nativeElement;
+    textArea.style.overflow = 'hidden';
+    textArea.style.height = '0px';
+    textArea.style.height = textArea.scrollHeight + 'px';
   }
 }
