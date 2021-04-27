a829703b0cf5306dfc641b67d9533ee9f1905b38
Tevin Gray <tevingray@Tevins-MacBook-Pro.local>
2021-04-27 08:50:33 -0700

    Created posts Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WritepostComponent } from './components/writepost/writepost.component';
 import { ReadpostComponent } from './components/readpost/readpost.component';
+import { PostsService } from './posts.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ReadpostComponent } from './components/readpost/readpost.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PostsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
