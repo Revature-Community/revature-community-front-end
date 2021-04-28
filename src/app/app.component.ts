6e93584dcbeafdbf028feae4b8b241a4b7343829
Brian Kinney <bkinney0514@gmail.com>
2021-04-28 10:31:57 -0400

    fixed a previous merge conflict, please don't push to main yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,9 @@ import { PostsService } from './posts.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // title = 'RevatureCommunity';
-<<<<<<< HEAD
-  // todaydate!: Date;
-  // constructor(private postsservice: PostsService){}
-  // ngOnInit() {
-  //   this.todaydate = this.postsservice.showTodayDate();
-  // }
+
   constructor() {};
   ngOnInit(){
-=======
-  // todaydate:any;
-
-  //This was in the constructor why? (private postsservice: PostsService)
-  constructor(){}
-  ngOnInit() {
-    // this.todaydate = this.postsservice.showTodayDate();
-  }
->>>>>>> a0576811e6dde5cceb3506ca123b60e5220e0ad5
 
   }
 }
