142a39a3b53d177f67070e6421d10dc6dfcfa8c3
Tevin Gray <tevingray@Tevins-MacBook-Pro.local>
2021-04-28 09:45:39 -0700

    submit working

--- src/app/posts/writepost/writepost.component.ts
+++ src/app/posts/writepost/writepost.component.ts
@@ -22,16 +22,70 @@ export class WritepostComponent implements OnInit {
   randomPost: Posts = {title: this.title, content: this.content, location: this.randomLocation, categoryType: this.categoryType}
  
   submitPost(){
-    this.randomLocation= new Locations(this.locationValue);
+    this.randomLocation= new Locations(this.locationValue++);
     this.randomPost = {title: this.title, content: this.content, location: this.randomLocation, categoryType: this.categoryType}
 
     console.log(this.randomPost);
-    console.log(this.title);
-    console.log(this.content);
     console.log(this.locationValue);
     this._post.submitPost(this.randomPost).subscribe(data => {
       console.log(data);
     })
   }
+
+  Locations = ["Alaska",
+    "Alabama",
+    "Arkansas",
+    "American Samoa",
+    "Arizona",
+    "California",
+    "Colorado",
+    "Connecticut",
+    "District of Columbia",
+    "Delaware",
+    "Florida",
+    "Georgia",
+    "Guam",
+    "Hawaii",
+    "Iowa",
+    "Idaho",
+    "Illinois",
+    "Indiana",
+    "Kansas",
+    "Kentucky",
+    "Louisiana",
+    "Massachusetts",
+    "Maryland",
+    "Maine",
+    "Michigan",
+    "Minnesota",
+    "Missouri",
+    "Mississippi",
+    "Montana",
+    "North Carolina",
+    "North Dakota",
+    "Nebraska",
+    "New Hampshire",
+    "New Jersey",
+    "New Mexico",
+    "Nevada",
+    "New York",
+    "Ohio",
+    "Oklahoma",
+    "Oregon",
+    "Pennsylvania",
+    "Puerto Rico",
+    "Rhode Island",
+    "South Carolina",
+    "South Dakota",
+    "Tennessee",
+    "Texas",
+    "Utah",
+    "Virginia",
+    "Virgin Islands",
+    "Vermont",
+    "Washington",
+    "Wisconsin",
+    "West Virginia",
+    "Wyoming"]
 
 }
