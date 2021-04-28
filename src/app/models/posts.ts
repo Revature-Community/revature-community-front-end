426d70a4b2109a369cacb70ba57bf82e93ba120b
David Carrillo <carrillo.dact13@gmail.com>
2021-04-28 11:23:23 -0700

    merging main

--- src/app/models/posts.ts
+++ src/app/models/posts.ts
@@ -2,10 +2,10 @@ import { Locations } from "./locations";
 
 export class Posts {
     constructor(
-    // public id: number,
-    public title: string,
-    public content: string,
-    public location: Locations, //change to location type later
-    public categoryType: string //change to category type later
+        public title: string,
+        public content: string,
+        public location: Locations, //change to location type later
+        public categoryType: string, //change to category type later
+        public id?: number
 )
 {}}
