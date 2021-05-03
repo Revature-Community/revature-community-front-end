e33d76560ff87825ef56a12fc91bd9b14210a0e3
AnderTwelve <willdabeastlol@gmail.com>
2021-05-03 15:30:38 -0500

    minor testing updates

--- src/app/location.service.spec.ts
+++ src/app/location.service.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpTestingController } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { LocationService } from './location.service';
 
 
@@ -8,6 +8,8 @@ describe('LocationService', () => {
   let service: LocationService;
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
+
+  let baseurl: "localhost:8085/locations"
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -24,4 +26,14 @@ describe('LocationService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return saved locations'), inject([HttpClient, HttpTestingController], (http: HttpClient, httpMock: HttpTestingController) => {
+    const locations = 
+        {"city": "Houston", 
+         "state":"Texas"};
+
+    http.get(baseurl)
+    service.getLocations()
+        
+  })
 });
