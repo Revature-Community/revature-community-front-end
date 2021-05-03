ba5ba33ddd4ff8d879d0faf11767e626a265d830
David Carrillo <carrillo.dact13@gmail.com>
2021-05-03 14:36:50 -0700

    alter post model to match server

--- src/app/posts/writepost/writepost.component.ts
+++ src/app/posts/writepost/writepost.component.ts
@@ -40,14 +40,16 @@ export class WritepostComponent implements OnInit {
 
   submitPost() {
     let postLocation = new Locations(this.locationForPosts, "")
+    let x = parseInt(localStorage.getItem("userId"));
     this.userPost = {
       title: this.title,
       content: this.content,
       locationId: postLocation,
       categoryType: this.categoryType,
-      id: parseInt(localStorage.getItem("userId")),
+      userId: x,
       username: localStorage.getItem("username")
     };
+    console.log("user id:" + localStorage.getItem("userId") + " and " + x);
 
     // Console loggin info
     console.log(this.userPost);
@@ -66,6 +68,7 @@ export class WritepostComponent implements OnInit {
   locationdata: any = [];
 
   states = [
+    'Alaska',
     'Alabama',
     'Arkansas',
     'Arizona',
